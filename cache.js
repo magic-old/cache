58adbe1b443458fd9152d6e1cd9b2e0f1f20c90b
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-07 22:58:44 +0100

    added lib dir, added dependencies

--- cache.js
+++ cache.js
@@ -4,22 +4,11 @@ var path     = require('path')
   , fs       = require('fs')
   , program  = require('commander')
   , log      = require('magic-log')
+  , builder  = require('./lib')
 ;
 
 module.exports = function execute() {
   program.version('0.0.1');
 
-  program
-    .command('*')
-    .description('test command')
-    .action(function () {
-      log('test command');
-    })
-  ;
-
-  program.parse(process.argv);
-
-  function printHelp() {
-    console.log('help text');
-  }
+  builder();
 }
